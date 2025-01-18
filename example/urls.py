66ae50d5f403eee137d0eea2a669d15e7cdb1742
Abdusshh <abdullah.enes189@gmail.com>
2025-01-19 02:29:59 +0300

    Add the new views to the urls

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,11 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index
+from example.views import index, trigger_task_view, task_triggered_success_view
 
 
 urlpatterns = [
     path('', index),
+    path('trigger-task/', trigger_task_view),
+    path('task-triggered-success/', task_triggered_success_view),
 ]
